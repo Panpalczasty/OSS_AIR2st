030ff843f84cb605baa8207a1a41ef5b9dc7dc01
Panpalczasty <mat.gluch@wp.pl>
2022-05-30 10:28:15 +0200

    mpc

--- MPC/plotPos.m
+++ MPC/plotPos.m
@@ -6,17 +6,17 @@ function res = plotPos(id, pos, posref, ps)
     f.Position = [0 600 600 400];
 
 
-    plot(pos(:,1),pos(:,2), "k","LineWidth",1.2);
-    hold on;
     plot(posref(:,1), posref(:,2),"b--");
-    grid();
+    hold on;
+    grid on;
     plot(pos(1,1),pos(1,2), "b*");
+    plot(pos(:,1),pos(:,2), "k","LineWidth",1.2);
     xlabel("x[m]");
     ylabel("y[m]");
     xlim([-1.7 1.7])
     ylim([-1.7 1.7])
     title("Trajectory of robot");
-    legend("Trajectory", "Reference trajectory", "Start point");
+    legend("Reference trajectory", "Start point", "Trajectory");
 
     x01 = ps.x0(1); 
     x02 = ps.x0(2);
